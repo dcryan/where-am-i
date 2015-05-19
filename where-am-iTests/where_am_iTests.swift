7e9b4926ae79481b7664c82e9c263cef37a6c3fd
Daniel Ryan <daniel.c.ryan@icloud.com>
2015-05-18 21:31:26 -0600

    Initial Commit

---
+++ where-am-iTests/where_am_iTests.swift
@@ -0,0 +1,36 @@
+//
+//  where_am_iTests.swift
+//  where-am-iTests
+//
+//  Created by Daniel Ryan on 5/18/15.
+//  Copyright (c) 2015 Daniel Ryan. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class where_am_iTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
